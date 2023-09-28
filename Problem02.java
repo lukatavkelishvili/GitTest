763283144644565b403790089487813909e79d18
S <s>
2023-09-28 18:53:21 +0400

    {"types":"C","runs":6}

--- Problem02.java
+++ Problem02.java
@@ -1,4 +1,24 @@
+import stanford.karel.Karel;
 
-public class Problem02 {
-
+public class Problem02 extends Karel{
+	public void run(){
+		
+		move();
+		move();
+		move();
+		move();
+		pickBeeper();
+		pickBeeper();
+		pickBeeper();
+		pickBeeper();
+		pickBeeper();
+		pickBeeper();
+		pickBeeper();
+		pickBeeper();
+		pickBeeper();
+		pickBeeper();
+		move();
+		
+		
+	}
 }
