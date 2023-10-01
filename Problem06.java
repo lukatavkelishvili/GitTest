784057ecc8088cdc17393701c99d744077b05fb5
S <s>
2023-10-01 19:21:15 +0400

    {"types":"C","runs":26}

--- Problem06.java
+++ Problem06.java
@@ -3,6 +3,36 @@ import stanford.karel.Karel;
 public class Problem06 extends Karel {
 	
 	public void run() {
+		while (frontIsClear()) {
+		lineWalk();
+		turnLeft();
+		move();
+		turnLeft();
+		lineWalk();
+		turnRight();
+		move();
+		turnRight();
+		}
 		
+		
+		
+		
+	}
+	private void lineWalk() {
+		if (noBeepersPresent()) {
+			putBeeper();
+		}
+		while (frontIsClear()) {
+			move();
+		if (noBeepersPresent()) {
+			putBeeper();
+		}
+		}
+	
+	}
+	private void turnRight() {
+		turnLeft();
+		turnLeft();
+		turnLeft();
 	}
 }
