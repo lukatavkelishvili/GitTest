8277d4764e995ac556b9b0e2baff7a6770c46a7a
S <s>
2023-10-10 18:56:26 +0400

    {"types":"C","runs":5}

--- Problem12.java
+++ Problem12.java
@@ -1,4 +1,10 @@
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
 
-public class Problem12 {
+public class Problem12 extends GraphicsProgram {
+	public void run() {
+		GRect wall = new GRect(300, 100);
+		add(wall, 100, 500);
+	}
 
 }
