e100512daafeee284fd598dddc73121561c6a776
S <s>
2023-10-10 18:27:07 +0400

    {"types":"C","runs":1}

--- GraphicsPractice.java
+++ GraphicsPractice.java
@@ -1,4 +1,8 @@
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
 
-public class GraphicsPractice {
-
+public class GraphicsPractice extends GraphicsProgram {
+	public void run() {
+		GRect myRect = new GRect(100, 200);
+	}
 }
