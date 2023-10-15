8c4fe5ef429d3e7be27b96473f506022a585fd84
S <s>
2023-10-15 21:36:13 +0400

    {"types":"C","runs":16}

--- CheckerboardKarel.java
+++ CheckerboardKarel.java
@@ -1,9 +1,9 @@
 /*
  * File: CheckerboardKarel.java
  * ----------------------------
- * When you finish writing it, the CheckerboardKarel class should draw
- * a checkerboard using beepers, as described in Assignment 1.  You
- * should make sure that your program works for all of the sample
+ * The CheckerboardKarel class draws
+ * a checkerboard using beepers, as described in Assignment 1.  
+ * My program works for all of the sample
  * worlds supplied in the starter folder.
  */
 
