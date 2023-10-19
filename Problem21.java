b767ffc321adba3c976fd11520062bf21c98d9de
S <s>
2023-10-19 19:09:17 +0400

    {"types":"C","runs":11}

--- Problem21.java
+++ Problem21.java
@@ -1,4 +1,28 @@
+import acm.graphics.GLine;
+import acm.program.GraphicsProgram;
 
-public class Problem21 {
+public class Problem21 extends GraphicsProgram {
+	private static final int NUM_LINES = 11;
+	public void run() {
+		for (int i = 0; i < NUM_LINES; i++) {
+			addVerticalLine(i);
+			addHorizontalLine(i);
+		}
+		
+		
+	}
+	private void addHorizontalLine(int i) {
+		double y = i;
+		double x1 = 0;
+		double x2 = getWidth();
+		double height = (double)getHeight() / (NUM_LINES - 1);
+		
+		GLine line = new GLine(x1, y, x2, y);
+		add(line);
+	}
+	private void addVerticalLine(int i) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
