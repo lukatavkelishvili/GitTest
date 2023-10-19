ad5c49f07a580c3d023abd309192139091325dac
S <s>
2023-10-19 18:43:26 +0400

    {"types":"C","runs":3}

--- Problem20.java
+++ Problem20.java
@@ -1,4 +1,10 @@
+import acm.program.ConsoleProgram;
 
-public class Problem20 {
+public class Problem20 extends ConsoleProgram {
+	public void run() {
+		for (int i = 0; i < 5; i++) {
+			int a = readInt("Enter number: ");
+		}
+	}
 
 }
