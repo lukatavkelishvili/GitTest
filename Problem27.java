620e872f5324380181243f1b3c58c88af315467c
S <s>
2023-10-26 18:55:58 +0400

    {"types":"C","runs":1}

--- Problem27.java
+++ Problem27.java
@@ -1,4 +1,20 @@
+import acm.program.ConsoleProgram;
 
-public class Problem27 {
+public class Problem27 extends ConsoleProgram {
+	public void run() {
+		int a = readInt("Enter number: ");
+		int b = readInt("Enter power: ");
+		
+		println("number power: " + numberPower(a, b));
+	}
+	
+	private int numberPower(int a, int b) {
+		
+		
+		
+		
+
+	
+	}
 
 }
