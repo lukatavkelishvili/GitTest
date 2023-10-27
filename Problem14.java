03ed6b4cc350df7562df95ce14280007281eaaa3
S <s>
2023-10-28 03:11:36 +0400

    {"types":"C","runs":27}

--- Problem14.java
+++ Problem14.java
@@ -1,7 +1,31 @@
 import acm.program.GraphicsProgram;
 
 public class Problem14 extends GraphicsProgram {
+	private static final int FACE_WIDTH = 
 	public void run() {
+		drawRobot();
+		
+	}
+
+	private void drawRobot() {
+		drawFace();
+		drawMouth();
+		drawEyes();
+		
+	}
+
+	private void drawFace() {
+		
+		
+	}
+
+	private void drawMouth() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void drawEyes() {
+		// TODO Auto-generated method stub
 		
 	}
 
