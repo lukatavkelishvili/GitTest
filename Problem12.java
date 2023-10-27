30b5d3e8ba3f1383b06206d9908e0a58f4fd5d8d
S <s>
2023-10-28 00:40:53 +0400

    {"types":"A","runs":17}

--- Problem12.java
+++ Problem12.java
@@ -1,49 +1,35 @@
-import acm.graphics.GLine;
 import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 
 public class Problem12 extends GraphicsProgram {
-	private static final double HOUSE_WIDTH = 200;
-	private static final double HOUSE_HEIGHT = 200;
-	private static final double WINDOW_WIDTH = 200;
-	private static final double WINDOW_HEIGHT = 200;
+	private static final int WINDOW_HEIGHT = 200;
+	private static final int WINDOW_WIDTH = 100;
+	private static final int BODY_HEIGHT = 600;
+	private static final int BODY_WIDTH = 1000;
 	public void run() {
-		drawMidLine();
-		drawHouse();
-		
-		
-		//GRect wall = new GRect(300, 100);
-		//add(wall, 100, 300);
-		
-		//GLine head = new head()
-	}
-
-	private void drawHouse() {
-		drawWall();
-//		drawWindows();
+		drawBody();
 //		drawRoof();
+//		drawWindows();
 //		drawDoor();
 		
 	}
-
-	private void drawWall() {
-		GRect wall = new GRect(HOUSE_WIDTH, HOUSE_HEIGHT);
-		double x = getWidth() / 2 - HOUSE_WIDTH /2;
-		double y = getHeight() - HOUSE_HEIGHT;
-		add(wall, x, y);
+	private void drawDoor() {
+		// TODO Auto-generated method stub
 		
 	}
-
-	private void drawMidLine() {
-		double x1 = 0;
-		double y1 = getHeight() / 2;
-		double x2 = getWidth();
-		double y2;
-		GLine line = new GLine(x1, y1, x2, y2);
-		add(line);
+	private void drawWindows() {
+		// TODO Auto-generated method stub
 		
 	}
-
+	private void drawRoof() {
+		// TODO Auto-generated method stub
+		
+	}
+	private void drawBody() {
+		GRect walls = new GRect(BODY_WIDTH, BODY_HEIGHT);
+		add(walls, (getWidth() / 2) - (BODY_WIDTH / 2), (getHeight() / 2) - (BODY_HEIGHT / 2) );
+		
+	}
 }
 
 		
