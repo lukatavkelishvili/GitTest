0b1909330d849202dc20a1c3bb9643f34d066773
S <s>
2023-10-28 03:18:24 +0400

    {"types":"C","runs":28}

--- Problem14.java
+++ Problem14.java
@@ -1,7 +1,13 @@
+import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 
 public class Problem14 extends GraphicsProgram {
-	private static final int FACE_WIDTH = 
+	private static final int FACE_WIDTH = 300;
+	private static final int FACE_HEIGHT = 500;
+	private static final int MOUTH_WIDTH = 80;
+	private static final int MOUTH_HEIGHT = 30;
+	private static final int EYES_WIDTH = 80;
+	private static final int EYES_HEIGHT = 30;
 	public void run() {
 		drawRobot();
 		
@@ -15,6 +21,12 @@ public class Problem14 extends GraphicsProgram {
 	}
 
 	private void drawFace() {
+		GRect face = new GRect(FACE_WIDTH, FACE_HEIGHT);
+		
+		double x = getWidth() / 2 - FACE_WIDTH / 2;
+		double y = getHeight() / 2 - FACE_HEIGHT / 2;
+		
+		add(face, x, y);
 		
 		
 	}
