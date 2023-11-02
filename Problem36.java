901b9ea1728dd1594267fc8a005fd4835a6ec1e5
S <s>
2023-11-02 19:04:26 +0400

    {"types":"C","runs":7}

--- Problem36.java
+++ Problem36.java
@@ -1,4 +1,12 @@
+import acm.program.ConsoleProgram;
 
-public class Problem36 {
+//თქვენი ამოცანაა გააკეთოთ მონეტის აგდების სიმულაციები და დათვალოთ საშუალოდ
+//რამდენჯერ უნდა ავაგდოთ მონეტა რათა 3-ჯერ ზედიზედ ამოვიდეს ბორჯღალო.
+public class Problem36 extends ConsoleProgram {
+	private static final int NUM_EXPERIMENTS = 1000;
+	public void run() {
+		double sumOfFlips = 0;
+		
+	}
 
 }
