90610eed22d4df3de1c56612ba18723e74ec73e2
S <s>
2023-11-03 19:02:02 +0400

    {"types":"A","runs":28}

--- Problem23.java
+++ Problem23.java
@@ -2,18 +2,26 @@ import acm.program.ConsoleProgram;
 
 public class Problem23 extends ConsoleProgram {
 	public void run() {
-		int n = readInt("Enter number: ");
-		int sumN = sumN(n);
+//		int n = readInt("Enter number: ");
+//		int sumN = sumN(n);
+//		
+//		println("Sum of the nums 1...n is: " + sumN);
+//	}
+//
+//	private int sumN(int n) {
+//		int sum = 0;
+//		for (int i = 1; i <= n; i++) {
+//			sum += i;  // sum=sum+i
+//		}
+//		return sum;
 		
-		println("Sum of the nums 1...n is: " + sumN);
-	}
-
-	private int sumN(int n) {
-		int sum = 0;
-		for (int i = 1; i <= n; i++) {
-			sum += i;  // sum=sum+i
+		int a = readInt ("Enter integral number: ");
+		int b = 0;
+		for ( int i = 0; i < a; i++) {
+			b += i;
+			
 		}
-		return sum;
+		println(b);
 	}
 
 }
