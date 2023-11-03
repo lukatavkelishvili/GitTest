9712bebea872c98a8960fba166bd686391f80a39
S <s>
2023-11-03 17:45:23 +0400

    {"types":"C","runs":1}

--- Problem15.java
+++ Problem15.java
@@ -2,11 +2,16 @@ import acm.program.ConsoleProgram;
 
 public class Problem15 extends ConsoleProgram {
 	public void run() {
-		int firstNum = readInt("Enter first number: ");
-		int secondNum = readInt("Enter second number: ");
+//		int firstNum = readInt("Enter first number: ");
+//		int secondNum = readInt("Enter second number: ");
+//		
+//		double avg = (firstNum + secondNum) / 2.0;
+//		println(avg);
+//	}
+		int a = readInt ("Enter first number: ");
+		int b = readInt ("Enter second number: ");
+		int c = Math.max(a, b);
 		
-		double avg = (firstNum + secondNum) / 2.0;
-		println(avg);
+		println(c);
 	}
-
 }
