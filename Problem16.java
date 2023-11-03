b40973a34f610759637aa42ff1db29bfc9811b6f
S <s>
2023-11-03 17:57:36 +0400

    {"types":"C","runs":12}

--- Problem16.java
+++ Problem16.java
@@ -12,7 +12,7 @@ public class Problem16 extends ConsoleProgram {
 		int a = readInt ("Enter first number: ");
 		int b = readInt ("Enter second number: ");
 		int c = readInt ("Enter third number: ");
-		double harm =1 / (double)(1 / a + 1 / b + 1 / c);
+		double harm = 1 / (1 / a + 1 / b + 1 / c);
 		
 		println(harm);
 	}
