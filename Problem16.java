437a8505d866baf0029e78c5c9abd36d4843cb1d
S <s>
2023-11-03 17:52:57 +0400

    {"types":"A","runs":5}

--- Problem16.java
+++ Problem16.java
@@ -2,11 +2,18 @@ import acm.program.ConsoleProgram;
 
 public class Problem16 extends ConsoleProgram {
 	public void run() {
-		double a = readInt("Enter first number: ");
-		double b = readInt("Enter second number: ");
-		double c = readInt("Enter third number: ");
+//		double a = readInt("Enter first number: ");
+//		double b = readInt("Enter second number: ");
+//		double c = readInt("Enter third number: ");
+//		
+//		double harm = 1 / (1 / a + 1 / b + 1 / c);
+//		println(harm);
 		
-		double harm = 1 / (1 / a + 1 / b + 1 / c);
+		int a = readInt ("Enter first number: ");
+		int b = readInt ("Enter second number: ");
+		int c = readInt ("Enter third number: ");
+		double harm = (double)1 / (1 / a + 1 / b + 1 / c);
+		
 		println(harm);
 	}
 
