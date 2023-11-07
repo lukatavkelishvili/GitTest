b5a890671f9f5d73db8e5144fa71dcc9b88c91f5
S <s>
2023-11-07 18:56:59 +0400

    {"types":"C","runs":1}

--- Problem39.java
+++ Problem39.java
@@ -1,4 +1,22 @@
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
 
-public class Problem39 {
+public class Problem39 extends GraphicsProgram {
+	private static final int PAUSE_MS = 20;
+	private static final int CIRCLE_DIAMETER = 50;
+	public void run() {
+		GOval circle = new GOval(CIRCLE_DIAMETER, CIRCLE_DIAMETER);
+		add(circle, 0, getHeight() / 2);
+		
+		int vx = 5;
+		int vy = 0;
+		
+		while (circle.getX() <= getWidth() - CIRCLE_DIAMETER) {
+			circle.move(vx, vy);
+			pause(PAUSE_MS);
+			
+		}
+		
+	}
 
 }
