509840a0942030ceb26ff91f76d7f8d1dfe99dfc
S <s>
2023-11-12 22:47:39 +0400

    {"types":"C","runs":28}

--- Problem22.java
+++ Problem22.java
@@ -10,8 +10,9 @@ public class Problem22 extends GraphicsProgram {
 		for(int j = 0; j < NUM_CELLS; j++) {
 			for (int i = 0;i < NUM_CELLS; i++) {
 			GRect square = new GRect(squareLength, squareLength);
-			if ((i + j) % 2 == 1) {
-				square.setFilled(true);
+//			if ((i + j) % 2 == 1) {
+//				square.setFilled(true);
+			square.setFilled((i + j) % 2 == 1); 
 			}
 			
 			add(square, x, y);
