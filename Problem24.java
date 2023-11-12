6daac88d837fbd97b3ce9120fb12a6d6d85db69d
S <s>
2023-11-12 22:25:00 +0400

    {"types":"C","runs":27}

--- Problem24.java
+++ Problem24.java
@@ -3,9 +3,7 @@ import acm.program.ConsoleProgram;
 public class Problem24 extends ConsoleProgram {
 	public void run() {
 		int a = readInt ("Enter number: ");
-		int b = 0;
-		for (int i = 1; i <= a; i++) {
-			if (a / i = )
+		
 		}
 	}
 
--- Sddd.java
+++ Sddd.java
@@ -1,4 +1,39 @@
+import acm.program.ConsoleProgram;
 
-public class Sddd {
+public class Sddd extends ConsoleProgram{
+	public void run() {
+		int num = readPositiveInt("Enter n: ");
+		int sum  = calculateSum(num);
+		println("Sum of numbers 1..n is: " + sum);
+		
+		int numDivisors = numberOfDivisors(num);
+		println("Number of divisors is: " + numDivisors);
+	}
 
+	private int readPositiveInt(String text) {
+		int n = readInt(text);
+		while( n <= 0) {
+			println("Number should positive");
+			n = readInt(text);
+		}
+		return n;
+	}
+	
+	private int calculateSum(int n) {
+		int sum = 0;
+		for(int i = 1; i <= n; i++) {
+			sum += i; // sum = sum + i
+		}
+		return sum;	
+	}
+	
+	private int numberOfDivisors(int n) {
+		int num = 0;
+		for(int i = 1; i <= n; i++) {		
+			if (n % i == 0) { // % remainder
+				num += 1;
+			}
+		}
+		return num;
+	}
 }
