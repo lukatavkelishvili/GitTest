ce8659443e0f8801c064d82cc81873992f1cfc55
S <s>
2023-11-16 18:50:05 +0400

    {"types":"C","runs":1}

--- Problem45.java
+++ Problem45.java
@@ -4,22 +4,26 @@ public class Problem45 extends ConsoleProgram {
 	public void run() {
 		String phrase = readLine("Enter word: ");
 		char symbol = readLine("Enter symbol: ").charAt(0);
-		symbolRepetition();
+		symbolRepetition(phrase, symbol);
 		
 		
 	}
 
-	private int symbolRepetition() {
+	private int symbolRepetition(String phrase, char symbol) {
 		int a = 0;
 		for (int i = 0; i < phrase.length(); i++) {
-			if (symbol.equals(phrase.charAt(i))) {
+			if (symbol == (phrase.charAt(i))) {
 				a += 1;
 			}
-			return(a);
+			
 			
 			
 		}
+		return(a);
 		
 	}
 
 }
+// f(x )  = x * 2 + 4;
+// 
+// f ( 5 ) = 5 * 2 + 4
