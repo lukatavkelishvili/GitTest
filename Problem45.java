1a3ec187b7fd277308d9bd2a4c3bb1aa1a91bcea
S <s>
2023-11-16 18:43:26 +0400

    {"types":"C","runs":0}

--- Problem45.java
+++ Problem45.java
@@ -1,4 +1,25 @@
+import acm.program.ConsoleProgram;
 
-public class Problem45 {
+public class Problem45 extends ConsoleProgram {
+	public void run() {
+		String phrase = readLine("Enter word: ");
+		char symbol = readLine("Enter symbol: ").charAt(0);
+		symbolRepetition();
+		
+		
+	}
+
+	private int symbolRepetition(phrase) {
+		int a = 0;
+		for (int i = 0; i < phrase.length(); i++) {
+			if (symbol.equals(phrase.charAt(i))) {
+				a += 1;
+			}
+			return(a);
+			
+			
+		}
+		
+	}
 
 }
