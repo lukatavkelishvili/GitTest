5e3b105888df2a87064ed342907d959ac3d303aa
S <s>
2023-11-28 18:52:28 +0400

    {"types":"C","runs":5}

--- Problem51.java
+++ Problem51.java
@@ -1,4 +1,27 @@
+import java.util.StringTokenizer;
 
-public class Problem51 {
+import acm.program.ConsoleProgram;
+
+public class Problem51 extends ConsoleProgram {
+	public void run() {
+		String text = readLine("Enter text" );
+//		StringTokenizer tokenizer = new StringTokenizer(text);
+		String editedText = removeDuplicates(text);
+		println(editedText);
+	}
+	private String removeDuplicates(String text) {
+		String result = "";
+		for (int i = 0; i < text.length(); i++) {
+			char ch = text.charAt(i);
+			
+			if (!result.contains("" + ch) {
+				result += ch;
+			}
+			
+		}
+			
+		
+			return result;
+	}
 
 }
