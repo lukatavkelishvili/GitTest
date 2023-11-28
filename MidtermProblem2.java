cd893b37a1f32c300f3343073824a80f62ae01f6
S <s>
2023-11-28 19:11:17 +0400

    {"types":"C","runs":6}

--- MidtermProblem2.java
+++ MidtermProblem2.java
@@ -1,4 +1,27 @@
+//2. კონსოლიდან შემოდის ტექსტი. დაბეჭდეთ True თუ ტექსტში 
+//არსებული ყველა lowercase ინგლისური ასო ზრდადობით არის დალაგებული, False თუ არაა. 
+//ტექსტში სხვა სიმბოლოებიც შეიძლება შეგხვდეთ და იმათ ყურადღება არ მიაქციოთ.  
+//მაგ: “a bcAAA!” -> true, “” -> true “ABCDz,a” -> false (Practice02)
+//Tags: String, char, charOperations, isLowerCase   
 
-public class MidtermProblem2 {
+
+
+
+import acm.program.ConsoleProgram;
+
+
+public class MidtermProblem2 extends ConsoleProgram {
+	public void run() {
+		String text = readLine("Enter text: ");
+		boolean answer = symbolChecks(text);
+		println(answer);
+		
+	}
+	
+	private boolean symbolChecks(String text) {
+		String result = "";
+		return rootPaneCheckingEnabled;
+		
+	}
 
 }
