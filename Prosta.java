cbb53658097eb774546ef8ae30fb356846cbf732
S <s>
2023-11-30 22:24:56 +0400

    {"types":"C","runs":5}

--- Prosta.java
+++ Prosta.java
@@ -2,16 +2,11 @@ import acm.program.ConsoleProgram;
 
 public class Prosta extends ConsoleProgram {
 	public void run() {
-		int a = readInt("Enter number: ");
-		println("number's factorial is:" + factorial(a));
-	}
-
-	private int factorial(int a) {
-		int b = 1;
-		for (int i = 1; i <= a; i++) {
-			b *= i;
+		   int num = 5;
+		   cow(num);
 		}
-		return b;
-	}
 
-}
+		private void cow(int grass) {
+		   println(grass);
+		}
+	}
