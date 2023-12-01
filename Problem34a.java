238f1deda57a5804cbc0a96e452a22fd8a4399f0
S <s>
2023-12-01 21:22:26 +0400

    {"types":"C","runs":23}

--- Problem34a.java
+++ Problem34a.java
@@ -1,4 +1,24 @@
+import java.awt.Color;
 
-public class Problem34a {
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
+
+public class Problem34a extends GraphicsProgram {
+	private static final int CIRCLE_NUM = 1000;
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	public void run() {
+		for (int i = 0; i < CIRCLE_NUM; i++) {
+			double x = rgen.nextDouble(25, 200);
+			double y = rgen.nextDouble(25, 200);
+			double radius = rgen.nextDouble(25, 200);
+			Color color = rgen.nextColor();
+			
+			GOval circle = new GOval(x, y, 2 * radius, 2 * radius);
+			circle.setFilled(true);
+			circle.setColor(color);
+			
+			add(circle);		}
+	}
 
 }
