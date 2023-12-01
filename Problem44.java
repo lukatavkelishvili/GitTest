de153dbf1f4905ac2fd29863ae3c3851e6e0c548
S <s>
2023-12-02 03:31:49 +0400

    {"types":"C","runs":75}

--- Problem44.java
+++ Problem44.java
@@ -1,4 +1,18 @@
+import java.awt.event.MouseEvent;
 
-public class Problem44 {
+import acm.program.GraphicsProgram;
+
+//მაუსის კლიკზე იმავე ადგილზე ჩნდება ბურთი და იწყებს ვარდნას ძირს, თუ მაუსს სხვა
+//ადგილზე ვაკლიკებ ბურთი გადადის ახალ ადგილზე და იქედან იწყებს ვარდნას
+public class Problem44 extends GraphicsProgram {
+	public void run() {
+		addMouseListeners();
+	}
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		createCircle();
+		circleStartsFalling();
+		
+	}
 
 }
