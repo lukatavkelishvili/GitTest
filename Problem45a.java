c25c30253fd36b08bf9c835565ecb6063dd026da
S <s>
2023-12-02 01:19:52 +0400

    {"types":"C","runs":60}

--- Problem45a.java
+++ Problem45a.java
@@ -1,4 +1,20 @@
+//მომხმარებელს კონსოლიდან შეჰყავს ტექსტი და სიმბოლო, თქვენმა პროგრამამ უნდა იპოვოს
+//რამდენჯერ გვხვდება მოცემულ ტექსტში მოცემული სიმბოლო
 
-public class Problem45a {
+import acm.program.ConsoleProgram;
+
+public class Problem45a extends ConsoleProgram {
+	public void run() {
+		String text = readLine("Enter text: ");
+		char symbol = readLine("Enter symbol: ").charAt(0);
+		
+		int a = 0;
+		for (int i = 0; i < text.length();) {
+			if (text.charAt(i) == symbol) {
+				a += 1;
+			}
+		}
+		println("In your text your symbol is repetited " + a + " times.");
+	}
 
 }
