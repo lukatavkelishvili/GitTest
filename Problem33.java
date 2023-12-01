20958340a74b906d738f59155bf2cd0c16ff4af9
S <s>
2023-12-01 21:06:14 +0400

    {"types":"C","runs":16}

--- Problem33.java
+++ Problem33.java
@@ -1,4 +1,25 @@
+//დაწერეთ პროგრამა რომელიც კანვასზე დახატავს შემთხვევითი ფერის მქონე წრეწირს,
+//რაიდიუსიც შემთხვევით აირჩეით შეზღუდული რეინჯიდან.
 
-public class Problem33 {
+
+import java.awt.Color;
+
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
+
+public class Problem33 extends GraphicsProgram {
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	public void run() {
+		double radius = rgen.nextDouble(100, 150);
+		double x = rgen.nextDouble(50,100);
+		double y = rgen.nextDouble(100,125);
+		Color color = rgen.nextColor();
+		
+		GOval circle = new GOval(x, y, 2 * radius, 2 * radius);
+		circle.setFilled(true);
+		circle.setColor(color);
+		add(circle);
+	}
 
 }
