7bb848f7686dccf3ff1896185bed5a66cda03eee
S <s>
2023-12-01 17:19:42 +0400

    {"types":"C","runs":8}

--- Prosta.java
+++ Prosta.java
@@ -2,15 +2,28 @@ import acm.program.ConsoleProgram;
 
 public class Prosta extends ConsoleProgram {
 	public void run() {
-		String text = readLine("Enter text: ");
+//		String text = readLine("Enter text: ");
+//		
+//		String reversedText = "";
+//		for (int i = 0; i < text.length(); i++) {
+//			reversedText = text.charAt(i) + reversedText;
+//			
+//		}
+//		println("Your reversed text is: " + reversedText);
 		
-		String reversedText = "";
-		for (int i = 0; i < text.length(); i++) {
-			reversedText = text.charAt(i) + reversedText;
-			
+		String tavkela = "alo";
+		String gvarama = "alo";
+		
+		if (tavkela.equals(gvarama)) {
+			println("hallo");
+		}else {
+			println("naxui");
 		}
-		println("Your reversed text is: " + reversedText);
+		
+		
 	}
+		
+		
 }
 
 
