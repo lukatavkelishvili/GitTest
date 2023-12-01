4dfdce9a9b6d84a9860e8adae6528ddf8b1b966f
S <s>
2023-12-02 03:39:31 +0400

    {"types":"C","runs":77}

--- Problem44.java
+++ Problem44.java
@@ -9,24 +9,20 @@ public class Problem44 extends GraphicsProgram {
 	private static final int RADIUS = 50;
 	private static final int V_Y = 5;
 	private static final int DELAY = 20;
+	private GOval circle;
 	public void run() {
+		circle = new GOval(2 * RADIUS, 2 * RADIUS);
+		circle.setFilled(true);
 		addMouseListeners();
+		circle.move(0, V_Y);
+		pause(DELAY);
 	}
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		createCircle(e);
-		circleStartsFalling();
 		
-	}
-	private void circleStartsFalling() {
-		
-		
-	}
-	private void createCircle(MouseEvent e) {
-		GOval circle = new GOval(2 * RADIUS, 2 * RADIUS);
-		circle.setFilled(true);
 		add(circle, e.getX() + RADIUS, e.getY() + RADIUS);
 		
 	}
+	
 
 }
