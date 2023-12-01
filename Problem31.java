de52b173b72cdf4bc04e8b0bb1d843c4b831f894
S <s>
2023-12-02 03:51:54 +0400

    {"types":"C","runs":80}

--- Problem31.java
+++ Problem31.java
@@ -1,4 +1,23 @@
+import acm.program.ConsoleProgram;
 
-public class Problem31 {
+//დაწერეთ პროგრამა რომელიც დაადგენს შეყვანილი რიცხვი მარტივია თუ არა
+public class Problem31 extends ConsoleProgram {
+	public void run() {
+		int a = readInt("Enter number: ");
+		if (isSimple(a) == true) {
+			println("Your number is simple!");
+		}else {
+			println("Your number is't simple!");
+		}
+	}
+
+	private boolean isSimple(int a) {
+		for (int i = 2; i < a; i++) {
+			if (a % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
 
 }
