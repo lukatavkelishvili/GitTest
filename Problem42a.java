62dbc010c09d5e535a0693e9ae0809b3a92260e1
S <s>
2023-12-01 22:12:06 +0400

    {"types":"C","runs":38}

--- Problem42a.java
+++ Problem42a.java
@@ -1,4 +1,20 @@
+import java.awt.event.MouseEvent;
 
-public class Problem42a {
+import acm.program.GraphicsProgram;
+
+//Paint-ის მონაკვეთის ხატვის გაკეთება, ჯერ აჭერ მაუსს, ფიქსირდება ერთი წვერო, მერე
+//მაუსის მოძრაობისას მონაკვეთიც მოძრაობს რადგან მეორე წვერო არაა დაფიქსირებული და
+//კლიკით ფიქსირდება მეორე წვეროც.
+
+
+public class Problem42a extends GraphicsProgram {
+	public void run() {
+		addMouseListeners();
+	}
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		
+		
+	}
 
 }
