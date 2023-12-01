a4b1bf22160f8c24150f32ae8ab70fe3a8158c2d
S <s>
2023-12-02 02:22:14 +0400

    {"types":"C","runs":72}

--- Problem50.java
+++ Problem50.java
@@ -6,10 +6,10 @@ public class Problem50 extends ConsoleProgram {
 	public void run() {
 		String text = readLine("Enter text ");
 //		StringTokenizer tokenizer = new StringTokenizer(text);
-		StringTokenizer tokenizer = new StringTokenizer(text, ", .,!");
+		StringTokenizer tokenizer = new StringTokenizer(text, " ");
 		int count = 0;
 		while (tokenizer.hasMoreTokens()) {
-//			String token = tokenizer.nextToken();
+			
 			count++;
 		}
 		println(count);
