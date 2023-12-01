7665d84eb4895235fe2909f88a786282995c0a5e
S <s>
2023-12-01 22:45:52 +0400

    {"types":"C","runs":45}

--- Problem23a.java
+++ Problem23a.java
@@ -1,4 +1,20 @@
+//მომხმარებელს შეყავს მთელი რიცხვი n, პროგრამამ უნდა დაბეჭდოს 1 დან n მდე რიცხვების
+//ჯამი
 
-public class Problem23a {
+import acm.program.ConsoleProgram;
+
+public class Problem23a extends ConsoleProgram {
+	public void run() {
+		int n = readInt("Enter number: ");
+		println(sumN(n));
+		
+	}
+	private int sumN(int n) {
+		int a = 0;
+		for (int i = 1; i <= n; i++) {
+			a += i;
+		}
+		return a;
+	}
 
 }
