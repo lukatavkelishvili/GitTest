f8c2f697b492a304591420768135f276861d8db6
S <s>
2023-12-01 23:23:34 +0400

    {"types":"C","runs":51}

--- Problem27a.java
+++ Problem27a.java
@@ -1,4 +1,21 @@
+//მომხმარებელს შეყავს n და m დაბეჭდეთ n^m
 
-public class Problem27a {
+import acm.program.ConsoleProgram;
+
+public class Problem27a extends ConsoleProgram {
+	public void run() {
+		int n = readInt("Enter first number: ");
+		int m = readInt("Enter second number: ");
+		int power = numbersPower(n, m);
+		println(power);
+	}
+	
+	private int numbersPower(int n, int m) {
+		int a = 1;
+		for (int i = 0; i < m; i++) {
+			a *= n;
+		}
+		return a;
+	}
 
 }
