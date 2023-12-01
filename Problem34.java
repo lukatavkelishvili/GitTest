419b8c69ad6eaf900c9dbc0c6ffae3832a35e178
S <s>
2023-12-01 21:40:40 +0400

    {"types":"C","runs":29}

--- Problem34.java
+++ Problem34.java
@@ -1,6 +1,5 @@
-//
-//
-//import acm.graphics.GOval;
+
+
 import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
@@ -10,80 +9,28 @@ import javafx.scene.paint.Color;
 //წრე, შემთხვევით კოორდინატებზე.
 
 
-//public class Problem34 extends GraphicsProgram {
-//	private static final int MIN_RADIUS = 10;
-//	private static final int MAX_RADIUS = 30;
-//	RandomGenerator rgen = RandomGenerator.getInstance();
-//	public void run() {
-////		RandomGenerator rgen = RandomGenerator.getInstance();
-////		
-////		boolean heads = rgen.nextBoolean(0.66);
-//		for (int i = 0; i < 1000; i++) {
-//			addRandomCircle();
-//		}
-//	}
-//	
-//	private void addRandomCircle() {
-//		int radius = rgen.nextInt(MIN_RADIUS, MAX_RADIUS);
-//		double x = rgen.nextDouble(0, getWidth() - 2 * radius);
-//		double y = rgen.nextDouble(0, getHeight() - 2 * radius);
-//		java.awt.Color color = rgen.nextColor();
-//		
-//		GOval circle = new GOval(2 * radius, 2 * radius);
-//		circle.setFilled(true);
-//		circle.setColor(color);
-//		add(circle, x, y);
-//	}
-//}
-
-import java.awt.Color;
-
-//კანვასზე დახატეთ 1000 შემთხვევითი რადიუსის მქონე შემთხვევითი ფერით გაფერადებული
-//წრე, შემთხვევით კოორდინატებზე.
-
 public class Problem34 extends GraphicsProgram {
-	private static final int CIRCLE_NUM = 1000;
-	private RandomGenerator rgen = RandomGenerator.getInstance();
+	private static final int MIN_RADIUS = 10;
+	private static final int MAX_RADIUS = 30;
+	RandomGenerator rgen = RandomGenerator.getInstance();
 	public void run() {
-		for (int i = 0; i < CIRCLE_NUM; i++) {
-			double x = rgen.nextDouble(25, 200);
-			double y = rgen.nextDouble(25, 200);
-			double radius = rgen.nextDouble(25, 200);
-			Color color = rgen.nextColor();
-			
-			GOval circle = new GOval(x, y, 2 * radius, 2 * radius);
-			circle.setFilled(true);
-			circle.setColor(color);
-			
-			add(circle);
+//		RandomGenerator rgen = RandomGenerator.getInstance();
+//		
+//		boolean heads = rgen.nextBoolean(0.66);
+		for (int i = 0; i < 1000; i++) {
+			addRandomCircle();
 		}
+	}
+	
+	private void addRandomCircle() {
+		int radius = rgen.nextInt(MIN_RADIUS, MAX_RADIUS);
+		double x = rgen.nextDouble(0, getWidth() - 2 * radius);
+		double y = rgen.nextDouble(0, getHeight() - 2 * radius);
+		java.awt.Color color = rgen.nextColor();
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		GOval circle = new GOval(2 * radius, 2 * radius);
+		circle.setFilled(true);
+		circle.setColor(color);
+		add(circle, x, y);
 	}
 }
