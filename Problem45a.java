b802ce9ec98682f5903900c0c3ee2547caa82d3a
S <s>
2023-12-02 01:23:09 +0400

    {"types":"C","runs":63}

--- Problem45a.java
+++ Problem45a.java
@@ -8,13 +8,13 @@ public class Problem45a extends ConsoleProgram {
 		String text = readLine("Enter text: ");
 		char symbol = readLine("Enter symbol: ").charAt(0);
 		
-		int a = 0;
-		for (int i = 0; i < text.length();) {
-			if (text.charAt(i) == symbol) {
-				a += 1;
-			}
-		}
-		println("In your text your symbol is repetited ");
+//		int a = 0;
+//		for (int i = 0; i < text.length();) {
+//			if (text.charAt(i) == symbol) {
+//				a += 1;
+//			}
+//		}
+		println("In your text your symbol is repetited " + a + " times.");
 	}
 
 }
