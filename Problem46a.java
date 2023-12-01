3911a250f8dc6b145f9cbd22c978d62d482635c4
S <s>
2023-12-02 01:50:50 +0400

    {"types":"C","runs":66}

--- Problem46a.java
+++ Problem46a.java
@@ -1,4 +1,32 @@
+import acm.program.ConsoleProgram;
 
-public class Problem46a {
+//დაწერეთ მეთოდი რომელსაც გადაეცემა სტიგნი და რომელიც აბრუნებს ამ სტრინგში
+//ყველაზე ხშირ სიმბოლოს.
+public class Problem46a extends ConsoleProgram {
+	public void run() {
+		String text = readLine("Enter text: ");
+		char common = mostCommonChar(text);
+		println("In your text most common symbol is: " + common);
+	}
+
+	private char mostCommonChar(String text) {
+		
+		int prev = 0;
+		int now = 0;
+		char more = text.charAt(0);
+		
+		for (int i = 0; i < text.length(); i++) {
+			if (now > prev) {
+			now = prev;
+			}
+			if (more == text.charAt(i)) {
+				now += 1;
+			}
+			if (now > prev) {
+				more = text.charAt(i);
+			}
+		}
+			
+	}
 
 }
