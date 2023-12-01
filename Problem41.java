f2ed4c22edf585d564955f6c005b80e8808bb447
S <s>
2023-12-01 22:02:18 +0400

    {"types":"C","runs":31}

--- Problem41.java
+++ Problem41.java
@@ -1,4 +1,23 @@
+//გააკეთეთ Paint-ის ფანქრის ანალოგიური ხელსაწყო. კერძოდ, მაუსის ყოველ მოძრაობაზე
+//ეკრანზე დაამატეთ გაფერადებული წრეწირები იმავე წერტილში სადაც მაუსი მდებარეობს.
 
-public class Problem41 {
+import java.awt.event.MouseEvent;
+
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
+
+public class Problem41 extends GraphicsProgram {
+	private static final int RADIUS = 20;
+	public void run() {
+		addMouseListeners();
+	}
+	public void MouseMoved(MouseEvent e) {
+		addCircle(e.getX(), e.getY());
+	}
+	public void addCircle(double x, double y) {
+		GOval circle = new GOval(RADIUS * 2, RADIUS * 2);
+		circle.setFilled(true);
+		add(circle, x - RADIUS, y - RADIUS);
+	}
 
 }
