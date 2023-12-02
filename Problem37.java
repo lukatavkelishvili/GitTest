14cd31d325ddb543b9e14a1f4e7e4ffd3388e290
S <s>
2023-12-02 13:12:00 +0400

    {"types":"C","runs":110}

--- Problem37.java
+++ Problem37.java
@@ -1,4 +1,26 @@
+import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator;
 
-public class Problem37 {
+//რულეტკა, მომხმარებელს შეყავს 0 დან 36 მდე რიცხვი, შემდეგ ხდება რულეტკის
+//დატრიალების სიმულაცია, კონსოლში უნდა დაიბეჭდოს ამოსული რიცხვი და მოიგო თუ არა
+//დადებულმა რიცხვმა
+public class Problem37 extends ConsoleProgram {
+	RandomGenerator rgen = RandomGenerator.getInstance();
+	public void run() {
+		int a = readInt("Enter num between 0 to 36: ");
+		if (a > 36 || a < 0) {
+			println("Enter valid number! ");
+			a = readInt("Enter num between 0 to 36: ");
+		}
+		int result = rgen.nextInt(0, 37);
+		println("Chosen number is: " + result);
+		if (result == a) {
+			println("You won!");
+		}else {
+			println("You lost!");
+		}
+			
+		
+	}
 
 }
