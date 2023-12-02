8add4b85fbd3f6ecf35f9ff4149a5e0bea970d94
S <s>
2023-12-02 18:42:21 +0400

    {"types":"C","runs":118}

--- Prosta.java
+++ Prosta.java
@@ -1,13 +1,25 @@
 
 //36. თქვენი ამოცანაა გააკეთოთ მონეტის აგდების სიმულაციები და დათვალოთ საშუალოდ
 //რამდენჯერ უნდა ავაგდოთ მონეტა რათა 3-ჯერ ზედიზედ ამოვიდეს ბორჯღალო.
+import acm.graphics.GRect;
 import acm.program.ConsoleProgram;
+import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
 
-public class Prosta extends ConsoleProgram {
+public class Prosta extends GraphicsProgram {
 	public void run() {
-		String a = "aBahsjR";
-		a = a.toLowerCase();
-		println(a);
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				double length = Math.min(getWidth(), getHeight()) / 8;
+				GRect a = new GRect(length, length);
+				if (i + j % 2 == 1) {
+					a.setFilled(true);
+				}
+				double x = j * length;
+				double y = i * length;
+				add(a);
+				
+			}
+		}
 	}
 }
