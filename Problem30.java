ca1f069d4790676499eefecc11202a8b9b303827
S <s>
2023-12-02 04:37:47 +0400

    {"types":"C","runs":86}

--- Problem30.java
+++ Problem30.java
@@ -1,4 +1,23 @@
+import acm.program.ConsoleProgram;
 
-public class Problem30 {
+//მომხმარებელს შეყავს მთელი რიცხვი, დაბეჭდეთ რიცხვი რომელიც მიიღება ციფრების
+//შებრუნებით, 1256 -> 6521
+public class Problem30 extends ConsoleProgram {
+	public void run() {
+		int a = readInt("Enter number: ");
+		
+		int reversedNumber = reversed(a);
+		println("Reversed number is: " + reversedNumber);
+	}
+
+	private int reversed(int a) {
+		int result = 0;
+		while (a != 0) {
+			int lastDigit = a % 10;
+			a /= 10;
+			result = result * 10 + lastDigit;
+		}
+		return result;
+	}
 
 }
