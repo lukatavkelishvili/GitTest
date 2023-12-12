12c2250afd7e98976a70e1ad60b4e0d0d6e28ba7
S <s>
2023-12-12 19:01:19 +0400

    {"types":"C","runs":3}

--- Problem57.java
+++ Problem57.java
@@ -1,4 +1,36 @@
 
-public class Problem57 {
+
+import java.util.Arrays;
+
+import acm.program.ConsoleProgram;
+
+public class Problem57 extends ConsoleProgram {
+	public void run() {
+		String a = readLine("Enter first string: ");
+		String b = readLine("Enter second string: ");
+		if (anagram(a, b)) {
+			println("True");
+		}else {
+			println("False");
+		}
+	}
+
+	private boolean anagram(String a, String b) {
+		if (Arrays.equals(getCountArray(a), getCountArray(b))) {
+			return true;
+		}
+		return false;
+	}
+	
+	private int[] getCountArray(String text) {
+		int[] counts = new int[26];
+		for (int i = 0; i < text.length(); i++) {
+			char c = text.charAt(i);
+			int index = c - 'a';
+			counts[index]++;
+		}
+		
+		return counts;
+	}
 
 }
