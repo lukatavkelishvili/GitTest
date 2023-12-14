460ee2206afa2974a7aeb390a812a1395efa473d
lukatavkelishvili <ltavk21@freeuni.edu.ge>
2023-12-14 19:12:18 +0400

    Add new files

---
+++ Problem29.java
@@ -0,0 +1,17 @@
+//მოხმარებელს შეყავს მთელი რიცხვი n, პროგრამამ უნდა დაბეჭდოს ფიბონაჩის მიმდევრობის
+//მე n წევრი.
+
+//import acm.program.ConsoleProgram;
+//
+//public class Problem29 extends ConsoleProgram {
+//	public void run() {
+//		int n = readInt("Enter number: ");
+//		int pibN = pibonachN(n);
+//		println("Pibonach n number is: " + pibN);
+//	}
+//	private int pibonachN(int n) {
+//		
+//		
+//	}
+//
+//}
